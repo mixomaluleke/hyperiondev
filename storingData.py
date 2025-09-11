484c2f551a7baccf2de7d1f32be33313d71216c0
mixomaluleke <75676128+mixomaluleke@users.noreply.github.com>
2025-09-11 20:34:17 +0200

    Add files via upload
    this is task 3

---
+++ storingData.py
@@ -0,0 +1,24 @@
+# Pseudocode:
+# 1. Ask the user for their name
+# 2. Ask the user for their age
+# 3. Ask the user for their house number
+# 4. Ask the user for their street name
+# 5. Print a sentence containing all the details of the user
+
+# Python code:
+
+# Ask the user for their name
+name = input("Please enter your name: ")
+
+# Ask the user for their age
+age = input("Please enter your age: ")
+
+# Ask the user for their house number
+house_number = input("Please enter your house number: ")
+
+# Ask the user for their street name
+street_name = input("Please enter your street name: ")
+
+# Print a sentence containing all the details of the user
+print(f"This is {name}.They are {age} years old and live at house number\
+       {house_number} on {street_name}.")
