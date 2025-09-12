c8d881103b347bbca5641cec02d4c2c57123ce82
mixomaluleke <75676128+mixomaluleke@users.noreply.github.com>
2025-09-12 20:53:27 +0200

    Add files via upload
    task 4 storing data in python

---
+++ storing variables.py
@@ -0,0 +1,24 @@
+# Pseudocode:
+# 1. Ask the user for his/her name
+# 2. Ask the user for his/her age
+# 3. Ask the user for his/her order number
+# 4. Ask the user for his/her order name
+# 5. Print all the details of the user
+
+# Python code:
+
+# Ask for his or her  name
+name = input("Please enter your name: ")
+
+# Ask  for his/her age
+age = input("Please enter your age: ")
+
+# Ask for the house number
+order_number = input("Please enter your order number: ")
+
+# Ask for the order name
+order_name = input("Please enter your order name: ")
+
+# this will print everything entered
+print(f"This is {name}.They are {age} years old and live at house number\
+       {order_number} on {order_name}.")
