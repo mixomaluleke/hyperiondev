90af4afbf15f46eed3f278670e4c52ebc43065b6
mixomaluleke <75676128+mixomaluleke@users.noreply.github.com>
2026-02-19 12:22:59 +0200

    Add files via upload

---
+++ Data Structures- The List.py
@@ -0,0 +1,20 @@
+# list of animals to use    
+Animal = ['goat', 'dog', 'chicken' , 'cow']
+
+# Print the first animal name
+print(f"First animal : {Animal[0]}")
+
+# Print the final animal name
+print(f"Last animal : {Animal[3]}")
+
+# Print the length of the list
+print(f"Length of list: {len(Animal)}")
+
+# numberof animals
+Number_of_Animals = [25, 15, 42]
+
+# print the number of animals 
+print(f"the number of {Animal[0]} is {Number_of_Animals[0]}")
+print(f"the number of {Animal[1]} is {Number_of_Animals[1]}")
+print(f"the number of {Animal[2]} is {Number_of_Animals[2]}")
+print(f"the number of {Animal[3]} is {Number_of_Animals[3]}")
